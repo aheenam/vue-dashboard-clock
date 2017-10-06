17aed59134f6c276d84529433415e7b614028f63
Rathes Sachchithananthan <rs@net-insiders.com>
2017-10-06 17:23:09 +0200

    Adds Lolex to fake timer/date

--- __tests__/clock.test.js
+++ __tests__/clock.test.js
@@ -1,23 +1,25 @@
 
 import { Clock } from '../src/index'
 import Vue from 'vue'
-import MockDate from 'mockdate'
-
-jest.useFakeTimers();
+import lolex from 'lolex'
 
 describe('dashboard-test', () => {
 
-  let Constructor = Vue.extend(Clock);
+  let Constructor = Vue.extend(Clock)
+  let clock = null
+
+  beforeEach(() => {
+    clock = lolex.install({
+      now: new Date('2018-01-01 12:00:00')
+    })
+  })
 
   it('should match the snapshot', () => {
-    MockDate.set('1/1/2018 12:00');
-
     let vm = new Constructor().$mount()
     expect(vm.$el).toMatchSnapshot()
   });
 
   it('should use timestamp if set', () => {
-    MockDate.set('1/1/2018 12:00:00');
 
     let vm = new Constructor({
       propsData: {
@@ -30,31 +32,20 @@ describe('dashboard-test', () => {
 
   })
 
-  // it('should run time', async () => {
-  //     MockDate.set('1/1/2018 12:00:00');
+  it('should run time', async () => {
 
-  //     let vm = new Constructor({
-  //         propsData: {
-  //             timezone: 'AMERICA/LOS_ANGELES'
-  //         }
-  //     }).$mount()
+      let vm = new Constructor({
+          propsData: {
+              timezone: 'AMERICA/LOS_ANGELES'
+          }
+      }).$mount()
 
-  //     expect(typeof vm.created).toBe('function')
+      clock.tick(1000)
+      await Vue.nextTick()
 
-  //     jest.runTimersToTime(1000);
-  //     await Vue.nextTick()
+      expect(vm.timezone).toEqual('AMERICA/LOS_ANGELES')
+      expect(vm.dateTime.format('HH:mm:ss')).toEqual('03:00:01')
 
-  //     expect(vm.timezone).toEqual('AMERICA/LOS_ANGELES')
-  //     expect(vm.dateTime.format('HH:mm:ss')).toEqual('03:00:01')
-
-  // })
-
-  afterEach(() => {
-    MockDate.reset()
   })
 
 })
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
